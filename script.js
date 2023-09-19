3548bb5505e54faf04c9c9694d5f86865170a1dd
Amina Magomedova <progristillidan@gmail.com>
2023-09-20 00:19:33 +0300

    second

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-// addEventListener('wheel') {}
-// window.scrollBy({
-//     top: 1
-// })
-// onscroll = (event) => {
-//     scrollTo(0, 1000);
-// };
+let books = [];
 
-document.addEventListener('wheel', function(event) {
-    event.preventDefault();
-    event.deltaY = 0;
-})
 
-// const syntheticEvent = new WheelEvent("syntheticWheel", {
-//     deltaY: 1000,
-// });
+function go() {
+    for (let i = 0; i < 5; i++) {
+        books.push({
+            title: 'book_' + books.length,
+            author: 'author_' + books.length,
+            grade: 0,
+            id: 'book_' + books.length,
+        })
+    }
+    render();
+}
+
+
+function render() {
+    for (book of books) {
+        document.getElementById('books').innerHTML += `
+        <div class="book">
+                <h6>${book.author}</h6>
+                <input type="image" src="" alt="add" class="input-image">
+                <h5>${book.title}</h5>
+                <div class="rating-area">
+                    <input type="radio" id="star-5" name="rating" value="5">
+                    <label for="star-5" title="Оценка «5»"></label>	
+                    <input type="radio" id="star-4" name="rating" value="4">
+                    <label for="star-4" title="Оценка «4»"></label>    
+                    <input type="radio" id="star-3" name="rating" value="3">
+                    <label for="star-3" title="Оценка «3»"></label>  
+                    <input type="radio" id="star-2" name="rating" value="2">
+                    <label for="star-2" title="Оценка «2»"></label>    
+                    <input type="radio" id="star-1" name="rating" value="1">
+                    <label for="star-1" title="Оценка «1»"></label>
+                    </div>
+                    </div>
+                    `
+    }
+}
+
+
+function search() {
+
+}
+
+function addBook() {
+
+}
